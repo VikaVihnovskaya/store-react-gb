ff5a84dd7f06ef33ce45cc49e0c417a9cc2e4d81
Vihnovskaya Vika <vihnovskaya1932@gmail.com>
2024-10-08 13:17:41 +0300

    add style and routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import 'style/style.scss'
 import './App.css';
+import {Route, Router, Routes} from "react-router-dom";
+import HomePage from "./components/HomePage/HomePage";
+import CatalogPage from "./components/CatalogPage/CatalogPage";
+import RegistrationPage from "./components/RegistrationPage/RegistrationPage";
+import CartPage from "./components/Cart/CartPage";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function ProductPage() {
+  return null;
 }
 
+function App() {
+  return(
+      <div>
+        <Router>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/product" element={<ProductPage />} />
+            <Route path="/cartPage" element={<CartPage />} />
+            <Route path="/registration" element={<RegistrationPage />} />
+            <Route path="/catalog" element={<CatalogPage />} />
+          </Routes>
+        </Router>
+      </div>
+  )
+}
 export default App;
